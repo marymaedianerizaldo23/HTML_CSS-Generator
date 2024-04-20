819007aac37096725438c20d5b063cbf7276070f
Hugh Vergara <hughmclude.vergara.21@usjr.edu.ph>
2024-04-20 14:28:38 +0800

    added launcher\main.php file

---
+++ main.php
@@ -0,0 +1,66 @@
+<?php
+
+require_once 'autoload.php';
+
+use Headings\HtmlStylerH1;
+use Headings\HtmlStylerH2;
+use Headings\HtmlStylerH3;
+use Headings\HtmlStylerH4;
+use Headings\HtmlStylerH5;
+use Headings\HtmlStylerH6;
+
+
+$html = file_get_contents('front.html');
+$styler = new HtmlStylerH1();
+$styler->addStyle('color', 'red')
+       ->addStyle('font-size', '24px')
+       ->addStyle('font-style', 'italic')
+       ->addStyle('text-decoration', 'underline')
+       ->addStyle('background-color', 'blue');
+$styler->applyStyles($html);
+file_put_contents('front.html', $html);
+
+// Load HTML file again
+$html = file_get_contents('front.html');
+$styler = new HtmlStylerH2();
+$styler->addStyle('color', 'blue')
+       ->addStyle('background-color', 'red');
+$styler->applyStyles($html);
+file_put_contents('front.html', $html);
+
+// Load HTML file again
+$html = file_get_contents('front.html');
+$styler = new HtmlStylerH1();
+$styler->setSelector('#try') 
+       ->addStyle('color', 'pink')
+       ->addStyle('background-color', 'white')
+       ->addStyle('font-size', '24px');
+$styler->applyStyles($html);
+file_put_contents('front.html', $html);
+
+
+$html = file_get_contents('front.html');
+$styler = new HtmlStylerH3();
+$styler->addStyle('color','green');
+$styler->applyStyles($html);
+file_put_contents('front.html', $html);
+
+$html = file_get_contents('front.html');
+$styler = new HtmlStylerH5();
+$styler->addStyle('color','purple');
+$styler->applyStyles($html);
+file_put_contents('front.html', $html);
+
+$html = file_get_contents('front.html');
+$styler = new HtmlStylerH4();
+$styler->addStyle('color','orange');
+$styler->applyStyles($html);
+file_put_contents('front.html', $html);
+
+
+$html = file_get_contents('front.html');
+$styler = new HtmlStylerH6();
+$styler->addStyle("color","orange")
+       ->addStyle("font-style","italic");
+$styler->applyStyles($html);
+file_put_contents("front.html",$html);
